52df6b0e8ad2247ee5561cf3b57fce6b387a84eb
Bryan Windsor <bryan.windsor@ordina.nl>
2019-10-11 13:40:31 +0200

    -integrated firebase auth into app -set default box-sizing to border-box -restyled sign in buttons -restyled header menu options -added conditional render of sign in and sign out based on firebase auth subscription

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in-styles.scss";
 
@@ -12,7 +12,7 @@ class SignIn extends Component {
     super(props);
     this.state = {
       email: "",
-      password: "",
+      password: ""
     };
   }
 
@@ -50,8 +50,12 @@ class SignIn extends Component {
             required
           />
 
-          <CustomButton type="submit"> Sign In </CustomButton>
-          <CustomButton onClick={signInWithGoogle}> Sign In With Google </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit"> Sign In </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign In With Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
