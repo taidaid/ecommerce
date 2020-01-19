0cdf618458ccee8f36d35f14fdec62a05a3c0d8c
Bryan <windsor.bryan@gmail.com>
2020-01-19 21:49:52 +0100

    changed single quotes to double, added styling, correctly set initial dropdown display

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import SHOP_DATA from './shop.data.js';
+import SHOP_DATA from "./shop.data.js";
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import CollectionPreview from "../../components/collection-preview/collection-preview";
 
 class ShopPage extends React.Component {
   constructor(props) {
@@ -16,7 +16,7 @@ class ShopPage extends React.Component {
   render() {
     const { collections } = this.state;
     return (
-      <div className='shop-page'>
+      <div className="shop-page">
         {collections.map(({ id, ...otherCollectionProps }) => (
           <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
